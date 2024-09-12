30b9f7709cdd02365a33e33ad39c1b893f4a9497
Jack Burnett <jackjburnett@gmail.com>
2024-09-12 16:14:15 +0100

    Testing Press and Release buttons

--- Assets/Scripts/SendViaUDP.cs
+++ Assets/Scripts/SendViaUDP.cs
@@ -6,6 +6,7 @@ using System.Text;
 public class SendViaUDP : MonoBehaviour
 {
     public Button sendButton;
+    public Button sendButton2;
     private UdpClient _udpClient;
     private string _serverIP = "192.168.0.17"; // Replace with your PC's IP address
     private int _port = 8080; // _port number for UDP communication
@@ -16,15 +17,34 @@ public class SendViaUDP : MonoBehaviour
         _udpClient = new UdpClient();
         
         // Add listener to the button
-        sendButton.onClick.AddListener(SendMessageToPC);
+        sendButton.onClick.AddListener(SendMessageToPC_A);
+        sendButton2.onClick.AddListener(SendMessageToPC_A2);
     }
 
-    void SendMessageToPC()
+    void SendMessageToPC_A()
     {
         try
         {
             // Create message
-            string message = "SPACEBAR";
+            string message = "A_PRESS";
+            byte[] data = Encoding.ASCII.GetBytes(message);
+
+            // Send message to the server
+            _udpClient.Send(data, data.Length, _serverIP, _port);
+            Debug.Log("Sent message to PC: " + message);
+        }
+        catch (SocketException e)
+        {
+            Debug.Log("SocketException: " + e);
+        }
+    }
+    
+    void SendMessageToPC_A2()
+    {
+        try
+        {
+            // Create message
+            string message = "A_RELEASE";
             byte[] data = Encoding.ASCII.GetBytes(message);
 
             // Send message to the server

0e8618bf7b455e672eeb96ead243b9206b513460
Jack Burnett <jackjburnett@gmail.com>
2025-02-09 23:26:08 +0000

    Created button spawner

--- Assets/Scripts/ButtonSpawner.cs
+++ Assets/Scripts/ButtonSpawner.cs
@@ -1,9 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class ButtonSpawner : MonoBehaviour
 {
+    public GameObject buttonPrefab; // Assign the prefab of your button in the Unity Editor
+    public RectTransform panel; // Assign the panel where you want to spawn the button
 
+    public void SpawnButton(string buttonFunction, GameObject udpServer)
+    {
+        // Instantiate the button prefab as a child of the panel
+        var button = Instantiate(buttonPrefab, panel);
+
+        // Get the ButtonBehavior component
+        var buttonBehavior = button.GetComponent<ButtonBehavior>();
+
+        // Set the button function value
+        buttonBehavior.buttonFunction = buttonFunction;
+
+        // Set the udp server game object
+        buttonBehavior.udpSender = udpServer.GetComponent<SendViaUDP>();
+    }
 }
---
+++ Assets/Scripts/SpawnButtonActivator.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class SpawnButtonActivator : MonoBehaviour
+{
+    // Reference to the ButtonSpawner script attached to the panel
+    public ButtonSpawner buttonSpawner;
+
+    // Reference to the SendViaUDP script attached to the EventSystem
+    public SendViaUDP sendViaUDP;
+
+    // Reference to the button behavior value
+    public string buttonBehaviorValue;
+
+    // Called when the button is clicked
+    public void OnButtonClick()
+    {
+        // Call the SpawnButton method of the ButtonSpawner script
+        buttonSpawner.SpawnButton(buttonBehaviorValue, sendViaUDP.gameObject);
+    }
+}

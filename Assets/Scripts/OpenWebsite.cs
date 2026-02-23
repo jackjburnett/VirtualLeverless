015833f57e9322baa1c0fdc2f7a5256b602c6f2b
Jack Burnett <jackjburnett@gmail.com>
2026-02-23 13:39:49 +0000

    v0.2
    v0.2 Release

---
+++ Assets/Scripts/OpenWebsite.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class OpenWebsite : MonoBehaviour
+{
+    public void Website()
+    {
+        Application.OpenURL("https://jackjburnett.github.io/VirtualLeverless/");
+    }
+}
